de0515c0e7ce7cb49c5275bde3972e46e029e713
Jeppe Pihl <jpihl08@gmail.com>
2019-03-01 14:19:59 +0100

    mvp packetizer approach

--- src/mts/packetizer.hpp
+++ src/mts/packetizer.hpp
@@ -10,15 +10,15 @@
 #include <vector>
 #include <functional>
 
+#include "buffer_view.hpp"
+
 namespace mts
 {
 /// Reads packets of abitray size and then constructs packets of
 /// 188 bytes - while ensuring the sync byte 0x47 is present.
-class packetizer
-{
-public:
 
-    using on_data_callback = std::function<void(const uint8_t*,uint64_t)>;
+struct packetizer
+{
 
 public:
 
@@ -34,85 +34,165 @@ public:
 
 public:
 
-    packetizer(on_data_callback on_data) :
-        m_on_data(on_data)
+    packetizer(std::function<void(const uint8_t *, uint32_t)> on_data) :
+        m_handle_data(on_data)
     {
-        assert(m_on_data);
-    }
-
-    void read(const uint8_t* data, uint64_t size)
-    {
-        assert(data != nullptr);
-        assert(size > 0);
-
-        // If leftover data and the incoming data is enough to release a packet
-        if (!m_buffer.empty() && ((m_buffer.size() + size) > packet_size()))
-        {
-            // The amount missing in the buffer to constitute a complete packet
-            auto delta = packet_size() - m_buffer.size();
-
-            // Is this a valid packet?
-            if (data[delta] == sync_byte())
-            {
-                // Add to buffer
-                m_buffer.insert(m_buffer.end(), data, data + delta);
-                data += delta;
-                size -= delta;
-
-                // Release packet
-                m_on_data(m_buffer.data(), m_buffer.size());
-            }
-            // Either the buffer was released or invalid
-            m_buffer.clear();
-        }
-
-        // Release as many packets as possible.
-        while (size >= packet_size())
-        {
-            // Check that the data is valid
-            if (data[0] == sync_byte())
-            {
-                // Release packet
-                m_on_data(data, packet_size());
-                data += packet_size();
-                size -= packet_size();
-            }
-            else
-            {
-                // Didn't find sync byte, advance the buffer
-                data += 1;
-                size -= 1;
-            }
-        }
-
-        // If the buffer already contains data, we don't need a sync byte.
-        if (m_buffer.empty())
-        {
-            // Look for end of data or sync byte
-            while (size > 0 && data[0] != sync_byte())
-            {
-                data += 1;
-                size -= 1;
-            }
-        }
-
-        // Store the data to be used for next call to read.
-        m_buffer.insert(m_buffer.end(), data, data + size);
+        assert(m_handle_data);
     }
 
     void reset()
     {
-        m_buffer.clear();
+        m_incomplete.clear();
     }
 
     uint64_t buffered() const
     {
-        return m_buffer.size();
+        return m_incomplete.size();
     }
 
-private:
+    void read(const uint8_t *data, uint32_t size)
+    {
+        buffer_view buffer{data, size};
 
-    const on_data_callback m_on_data;
-    std::vector<uint8_t> m_buffer;
+        if (has_incomplete())
+        {
+            // If the buffer is currently not empty we check if the incoming
+            // data can complete a previously incomplete packet
+            buffer = handle_incomplete(buffer);
+        }
+        buffer = sync(buffer);
+
+        if (buffer.is_empty())
+        {
+            return;
+        }
+
+        assert(buffer.in_sync());
+
+        // Deliver all the complete packets
+        for (uint32_t i = 0; i < buffer.packets(); ++i)
+        {
+            m_handle_data(buffer.packet_at(i), 188U);
+        }
+
+        // If we have some remaining data add it to the incomplete
+        if (buffer.has_partial_packet())
+        {
+            const uint8_t *partial_data = buffer.partial_packet_data();
+            uint32_t partial_size = buffer.partial_packet_size();
+
+            m_incomplete.insert(m_incomplete.end(),
+                                partial_data,
+                                partial_data + partial_size);
+        }
+    }
+
+    bool has_incomplete() const
+    {
+        return !m_incomplete.empty();
+    }
+
+    uint32_t missing_bytes() const
+    {
+        assert(m_incomplete.size() > 0);
+        assert(m_incomplete.size() < 188U);
+        return 188U - m_incomplete.size();
+    }
+
+    /// @return An empty or in sync buffer
+    buffer_view handle_incomplete(buffer_view buffer)
+    {
+        uint32_t missing = missing_bytes();
+
+        if (buffer.size() > missing)
+        {
+            // We know that the after consuming "missing" bytes from the data
+            // buffer we should see a sync byte - if not we cannot merge the
+            // buffer and have to give up and start over
+            buffer_view tmp = slide(buffer, missing);
+
+            if (tmp.in_sync())
+            {
+                // The sync bytes line up taking into account both the incomplete
+                // packet and the incoming data
+                m_incomplete.insert(m_incomplete.end(), buffer.data(), buffer.data() + missing);
+                m_handle_data(m_incomplete.data(), 188U);
+                m_incomplete.clear();
+
+                return tmp;
+            }
+            else
+            {
+                // The sync bytes were not found at the correct offsets. We have
+                // to assume something was lost. We restart the read with the
+                // incomplete buffer cleared.
+                m_incomplete.clear();
+                return sync(buffer);
+            }
+        }
+        else if (buffer.size() == missing)
+        {
+            // We release the buffer
+            m_incomplete.insert(m_incomplete.end(), buffer.data(), buffer.data() + missing);
+            m_handle_data(m_incomplete.data(), 188U);
+            m_incomplete.clear();
+
+            return buffer_view{};
+        }
+        else
+        {
+            // We did not have enough data to fill the incomplete packet
+            m_incomplete.insert(m_incomplete.end(), buffer.data(), buffer.data() + missing);
+            return buffer_view{};
+        }
+    }
+
+    buffer_view slide(buffer_view buffer, uint32_t offset) const
+    {
+        const uint8_t *data = buffer.data() + offset;
+        uint32_t size = buffer.size() - offset;
+
+        return buffer_view{data, size};
+    }
+
+    /// @return An empty or in sync buffer
+    buffer_view sync(buffer_view buffer) const
+    {
+        if (buffer.in_sync())
+        {
+            return buffer;
+        }
+
+        // Two cases
+        // 1. We have a partial or just one ts packet
+        // 2. We have data for multiple ts packets
+
+        // Case 1:
+        if (buffer.size() < 188U)
+        {
+            return buffer_view{};
+        }
+
+        // Case 2:
+        uint32_t max_offset = buffer.size() - 188U;
+
+        for (uint32_t offset = 0; offset <= max_offset; ++offset)
+        {
+            buffer_view tmp = slide(buffer, offset);
+
+            // Look for the intial sync byte
+            if (!tmp.in_sync())
+            {
+                continue;
+            }
+
+            return tmp;
+        }
+
+        return buffer_view{};
+    }
+
+    std::vector<uint8_t> m_incomplete;
+    std::function<void(const uint8_t *, uint32_t)> m_handle_data;
 };
 }
